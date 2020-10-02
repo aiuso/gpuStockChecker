723a23ea6f46cff22c6c455a0f3978af84f69277
Anthony Iuso <71988742+aiuso@users.noreply.github.com>
2020-10-02 19:58:03 -0400

    Add files via upload

---
+++ send.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+import http.client
+import datetime
+import os
+
+
+load_dotenv()
+
+
+##### Timestamp
+
+def timestamp():
+    return datetime.datetime.now().strftime("%c")
+
+
+##### Discord Messages
+
+def discord_msg(message):   # Code Credit: Chris Garrett
+    webhookurl = os.getenv('webhookurl')
+
+    # compile the form data (BOUNDARY can be anything)
+    formdata = "------:::BOUNDARY:::\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\n"\
+               + message + "\r\n------:::BOUNDARY:::--"
+
+    # get the connection and make the request
+    connection = http.client.HTTPSConnection("discordapp.com")
+    connection.request("POST", webhookurl, formdata, {
+        'content-type': "multipart/form-data; boundary=----:::BOUNDARY:::",
+        'cache-control': "no-cache",
+    })
+    # get response
+    response = connection.getresponse()
+    result = response.read()
+
+    # return back to the calling function with the result
+    return result.decode("utf-8")
+
+
+def scheduled_msg():
+    print(discord_msg('Still actively searching for your 3080... Stand by.'))
+
+
+
+
+
+
+
+
