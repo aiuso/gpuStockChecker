adacb505574537cc4b4831f148317fd3cfe04838
Anthony Iuso <71988742+aiuso@users.noreply.github.com>
2020-10-06 16:30:59 -0400

    Increase stringency for searches.

--- gpu_stockChecker_0.4.py
+++ gpu_stockChecker_0.4.py
@@ -27,43 +27,70 @@ print('____________\n')
 def search_evga():
     navigate.search(url.get('evga'))
     out_of_stock = g.driver.find_elements_by_id("LFrame_pnlOutOfStock") # elements will return empty list [] if element absent
+
     if out_of_stock:
         print(f'Out of Stock at EVGA. {send.timestamp()}')
+
     else:
-        print(f'{send.timestamp()}... {g.gpu} may be in stock at EVGA! Check url: {url.get("evga")}')
-        print(send.discord_msg(f'{send.timestamp()}... {g.gpu}'
-                               f' may be in stock at EVGA! Check url: {url.get("evga")}'))
-        g.stop_count += 1
+        print(f"EVGA 'Out of Stock' banner was not found.. checking add to cart. {send.timestamp()}")
+        add_to_cart = g.driver.find_elements_by_class_name("AddToChat")
+
+        if 'add' in add_to_cart[0].text.lower():
+            g.stop_count += 1
+            print(f"EVGA 'Add to Cart' button was found.. sending notification {send.timestamp()}.")
+            print(send.discord_msg(f'{send.timestamp()}... {g.gpu}'
+                                   f' may be in stock at EVGA! Check url: {url.get("evga")}'))
+        else:
+            print(f"EVGA 'Add to Cart' button was not found. {send.timestamp()}.")
 
     time.sleep(15)
 
 
 def search_bestbuy():
     navigate.search(url.get('bestbuy'))
-    out_of_stock = g.driver.find_elements_by_class_name('add-to-cart-button')
-    if out_of_stock:
-        if 'sold' in out_of_stock[0].text.lower():
+    button = g.driver.find_elements_by_class_name('add-to-cart-button')
+
+    if button:
+        if 'sold' in button[0].text.lower():
             print(f'Out of Stock at BestBuy. {send.timestamp()}')
-        else:
-            print(f'{send.timestamp()}... {g.gpu} may be in stock at BestBuy! Check url: {url.get("bestbuy")}')
+
+        elif 'coming' in button[0].text.lower():
+            print(f'Coming Soon at BestBuy. {send.timestamp()}')
+
+        elif 'add' in button[0].text.lower():
+            g.stop_count += 1
+            print(f"Bestbuy 'Add To Cart' button was found... sending message{send.timestamp()}")
             print(send.discord_msg(f'{send.timestamp()}... {g.gpu}'
                                    f' may be in stock at BestBuy! Check url: {url.get("bestbuy")}'))
-            g.stop_count += 1
+
+        else:
+            print(f'Unfamiliar text on button. {send.timestamp()}')
+
+    else:
+        print(f'Could not locate the BestBuy button/banner. {send.timestamp()}')
+
     time.sleep(15)
 
 
 def search_newegg():
     navigate.search(url.get('newegg'))
-    pointer = g.driver.find_elements_by_class_name('product-inventory')
+    banner = g.driver.find_elements_by_class_name('product-inventory')
+    button = g.driver.find_elements_by_class_name('btn-wide')
+
     try:
-        out_of_stock_msg = pointer[0].text.lower()
-        if 'out' in out_of_stock_msg:
+        if 'out' in banner[0].text.lower():
             print(f'Out of Stock at Newegg. {send.timestamp()}')
+
         else:
-            print(f'{send.timestamp()}... {g.gpu} may be in stock at Newegg! Check url: {url.get("newegg")}')
-            print(send.discord_msg(f'{send.timestamp()}... {g.gpu}'
-                                   f' may be in stock at Newegg! Check url: {url.get("newegg")}'))
-            g.stop_count += 1
+            if 'add' in button[0].text.lower():
+                g.stop_count += 1
+                print(f"Newegg 'Add To Cart' button was found... sending message. {send.timestamp()}")
+                print(send.discord_msg(f'{send.timestamp()}... {g.gpu}'
+                                       f' may be in stock at Newegg! Check url: {url.get("newegg")}'))
+
+            else:
+                print(f'OoS banner not found. AtC button not found on Newegg... {send.timestamp()}')
+
     except IndexError:
         print('Unable to check Newegg site...')
 
